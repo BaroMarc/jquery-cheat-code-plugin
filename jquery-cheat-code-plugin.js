33207b466fa256ea5da2c644de5a00d490fc7bca
Marc <marcbaro22@gmail.com>
2015-02-07 21:04:40 +0100

    Project clean-up + package.json

--- jquery-cheat-code-plugin.js
+++ jquery-cheat-code-plugin.js
@@ -46,18 +46,7 @@
 
     this.cheatController = new CheatCodeController();
 
-    function validateCheatSequence(sequence){
-
-    }
-
-    this.cheatSequence = function(sequence){
-      if(!sequence) return this.cheatController.sequence;
-      else{
-        this.cheatController.sequence = sequence; //TODO validate sequence
-        this.cheatController.state = 0;
-        this.cheatController.cheatOn = false;
-      }
-    };
+    var context = this;
 
     this.inputMapping = function(mapping){
       if(!mapping) return this.cheatController.mapping;
@@ -65,6 +54,31 @@
         this.cheatController.mapping = mapping; //TODO validate mapping
         this.cheatController.state = 0;
         this.cheatController.cheatOn = false;
+      }
+    };
+
+    function validateCheatSequence(sequence){
+      var mapping = context.inputMapping();
+      for(i=0;i<sequence.length;++i){
+        var found = false;
+        Object.keys(mapping).forEach(function (key) {
+          var val = mapping[key];
+          if(val == sequence[i]) found = true;
+        });
+        if(!found) return false;
+      }
+      return true;
+    }
+
+    this.cheatSequence = function(sequence){
+      if(!sequence) return this.cheatController.sequence;
+      else{
+        if(validateCheatSequence(sequence)){
+          this.cheatController.sequence = sequence;
+          this.cheatController.state = 0;
+          this.cheatController.cheatOn = false;
+        }
+        else throw(new Error('Invalid cheat sequence provided'));
       }
     };
 
@@ -88,8 +102,6 @@
     else if(typeof element == 'string'){
       element.object = $(element);
     }
-
-    var context = this;
 
     element.object.on('keyup',function(event){
       context.cheatController.advanceState(event.keyCode);
--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  var code = new CheatCodeEventHandler({object:'#input'});
-  code.cheatSequence(['down', 'down', 'left', 'up', 'B']);
-
-  $(document).on('cheatCodeEntered',function(){
-    alert('Awesome cheat code library! Sequence = ' + code.cheatSequence());
-  });
-});
